b079afdf9ab5e97b3893e8853dbd89e875e95844
Elemont <elemontfx@outlook.com>
2026-06-17 00:04:06 +1000

    Align contract pragmas with OpenZeppelin 5 requirement

--- contracts/TextNFT.sol
+++ contracts/TextNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.20;
 
 import {Base64} from "./MetadataUtils.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
